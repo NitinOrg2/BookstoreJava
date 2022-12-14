d249b65d32f1715cae5357917b28e4a7561b0fa3
KishorH <kishor.hasurkar@checkmarx.com>
2022-12-14 15:28:39 +0530

    This is push

--- Books_jsp.java
+++ Books_jsp.java
@@ -22,6 +22,12 @@ public class Books_jsp extends HttpJspBase {
   static final int adSearch_ = 4;
   static final int ad_Search_ = 5;
   static final String appPath   ="/";
+  static final int adText = 1;
+  static final int adDate = 2;
+  static final int adNumber = 3;
+  static final int adSearch_ = 4;
+  static final int ad_Search_ = 5;
+  static final String appPath   ="/";
 
 //Database connection string
 
