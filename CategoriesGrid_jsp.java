ec4530ef9d6d9847f2f1692e2d4e3cbeac71ae42
KishorHasurkar <111050485+KishorHasurkar@users.noreply.github.com>
2023-02-23 15:04:14 +0530

    Update CategoriesGrid_jsp.java

--- CategoriesGrid_jsp.java
+++ CategoriesGrid_jsp.java
@@ -16,6 +16,12 @@ public class CategoriesGrid_jsp extends HttpJspBase {
 static final String CRLF = "\r\n";
   static final String CRLF = "\r\n";
   static final String CRLF = "\r\n";
+    static final int adText = 1;
+  static final int adDate = 2;
+  static final int adNumber = 3;
+  static final int adSearch_ = 4;
+  static final int ad_Search_ = 5;
+  static final String appPath   ="/";
 
   static final int adText = 1;
   static final int adDate = 2;
