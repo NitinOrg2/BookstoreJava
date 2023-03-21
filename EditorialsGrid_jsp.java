019d5eebdc9bf6dc1c053a9b5417636d726e96c4
KishorHasurkar <111050485+KishorHasurkar@users.noreply.github.com>
2023-03-21 12:04:05 +0530

    Update EditorialsGrid_jsp.java

--- EditorialsGrid_jsp.java
+++ EditorialsGrid_jsp.java
@@ -16,12 +16,7 @@ public class EditorialsGrid_jsp extends HttpJspBase {
 
   static final int UNDEFINT=Integer.MIN_VALUE;
 
-  static final int adText = 1;
-  static final int adDate = 2;
-  static final int adNumber = 3;
-  static final int adSearch_ = 4;
-  static final int ad_Search_ = 5;
-  static final String appPath   ="/";
+
 
 //Database connection string
 
